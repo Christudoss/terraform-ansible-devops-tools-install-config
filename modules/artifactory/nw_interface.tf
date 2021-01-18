7c866c2763c96cd55ef3855b949a346445d91b8f
Christudoss <christudoss@gmail.com>
2021-01-18 17:55:27 +0530

    fixed network interface issue

--- modules/artifactory/nw_interface.tf
+++ modules/artifactory/nw_interface.tf
@@ -1,5 +1,6 @@
 resource "aws_network_interface" "devops" {
   subnet_id   = var.subnet_id[0]
+  security_groups = [aws_security_group.artifactory.id]
 #   private_ips = ["172.16.10.100"]
 
   tags = {
